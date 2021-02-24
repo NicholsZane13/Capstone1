bb7715406e5d367526e94d238887e5bbfe78536d
Zane Nichols <nichols.zane16@gmail.com>
2021-02-23 20:46:35 -0600

    Full push after struggling through git bash problems

---
+++ inventory-project/src/data.js
@@ -0,0 +1,245 @@
+
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Corvette - Distance Driver",
+    img: "img/Driver1Corvette.jpg",
+    price: 16.99,
+    company: "Innova",
+    info:
+      "The Corvette is a speed 14 driver that’s long on glide with just enough fade for extraordinary distance — think of it as a faster Shryke.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Rask - Distance Driver",
+    img: "img/Driver2Rask.png",
+    price: 17.99,
+    company: "Kastaplast",
+    info:
+      "Being less than 22 mm wide at the rim, Rask proves that a disc’s speed cannot be determined by looking at the width of the rim. Those who want the fastest disc available, without the widest rim, reach for Rask! Some players swear by it, but it sure isn’t for everyone. Rask is Swedish for fast.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Gladiator - Distance Driver",
+    img: "img/Driver3Gladiator.jpg",
+    price: 17.99,
+    company: "Latitude 64",
+    info:
+      "Heading for the drawing board when designing the high-speed Gladiator, we sought for superior glide and distance, along with overstability to make it a true and trustworthy companion for the pros.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Recoil - Distance Driver",
+    img: "img/Driver4Recoil.jpg",
+    price: 17.99,
+    company: "Latitude 64",
+    info:
+      "When you need the distance but you do not want to compromise on control, the Recoil fits the bill. Rated at speed 12, this overstable distance driver has a comfortable grip and the mild low speed fade makes it easier to stay on the fairway.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Sapphire - Fairway Driver",
+    img: "img/Fairway1Sapphire.jpg",
+    price: 13.99,
+    company: "Latitude 64",
+    info:
+      "It’s light, it’s fast, yet still easy to grip and control. With the Sapphire you will get that extra speed and stability in your disc arsenal. It has a slim profile for smooth releases, and it is easy to get a good grip, especially for those of you with smaller hands.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Pioneer - Fariway Driver",
+    img: "img/Fairway2Pioneer.jpg",
+    price: 17.99,
+    company: "Latitude 64",
+    info:
+      " The Pioneer is an overstable fairway driver made for reliability and consistency. It has a flat top which makes it a great choice for sidearms. With a good amount of speed and a strong fade it is a versatile tool in any bag.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Explorer - Fairway Driver",
+    img: "img/Fairway3Explorer.jpg",
+    price: 13.99,
+    company: "Latitude 64",
+    info:
+      "Designed to be the workhorse for all kinds of accurate fairway drives. This stable and versatile fairway driver is smooth out of the hand and has a clean stable flight.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Vandal - Fairway Driver",
+    img: "img/Fairway4Vandal.jpg",
+    price: 17.99,
+    company: "Dynamic Disc",
+    info:
+      "The Vandal flies like a longer Maverick, so it’s extremely versatile. Newer players will love the straighter lines they can throw, and players with more arm speed will enjoy it for easy turnovers or rollers. The Vandal is built to carve up any course with dependable turn and sneaky speed.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 9,
+    title: "Teebird - Fairway Driver",
+    img: "img/Fairway5Teebird.jpg",
+    price: 9.99,
+    company: "Innova",
+    info:
+      "The TeeBird is one of our most accurate and reliable fairway drivers. It has a combination of straight flight, high glide and good speed that make it accurate and long flying both upwind and downwind.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 10,
+    title: "Truth - Midrange",
+    img: "img/Midrange1Truth.jpg",
+    price: 17.99,
+    company: "Dynamic Disc",
+    info:
+      "Hyzer, straight, anhyzer, distance, approach, low ceiling power shots, high floating drop shots, all have been accomplished with the Truth.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 11,
+    title: "Justice - Midrange",
+    img: "img/Midrange2Justice.jpg",
+    price: 17.99,
+    company: "Dynamic Disc",
+    info:
+      "The Justice by Dynamic Discs is going to become your new utility disc. It will not turn over. Throw the Justice one time and you will not need to reach for anything else for a short distance, overstable answer.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 12,
+    title: "Lion - Midrange",
+    img: "img/Midrange3Lion.jpg",
+    price: 13.99,
+    company: "Innova",
+    info:
+      "The convex rim, small bead and flat flight plate give the Lion a comfortable grip and create a neutral flight. Thrown hard and flat, it’ll finish with a touch more glide than a Roc3 and enough fade to keep it on track.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 13,
+    title: "Keystone - Putter",
+    img: "img/Putter1Keystone.jpg",
+    price: 10.99,
+    company: "Latitude 64",
+    info:
+      "It has a beadless, rounded edge and medium depth. The flight path is understable. With the Keystone you will get easy turnovers and straight flights even at low speeds.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 14,
+    title: "Judge - Putter",
+    img: "img/Putter2Judge.jpg",
+    price: 9.99,
+    company: "Dynamic Disc",
+    info:
+      "It is incredibly predictable in flying and hitting the chains. It is a fantastic throwing putter that will fly straight and glide for amazing distances.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 15,
+    title: "Reko - Putter & Approach",
+    img: "img/Putter3Reko.jpg",
+    price: 14.99,
+    company: "Kastaplast",
+    info:
+      "Reko has a comfortable rounded profile and a smooth bead, which fits most players’ hands. Easy to grip and easy to throw. It features a strengthened shoulder which adds a little extra durability compared to the average putter.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 16,
+    title: "Colt - Putter",
+    img: "img/Putter4Colt.jpg",
+    price: 6.99,
+    company: "Innova",
+    info:
+      "The Colt is a low profile small diameter putt and approach disc similar to our XD or Classic Roc. It is a fairly straight flyer, that can also perform well for slow turnover shots that don't come crashing back.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 17,
+    title: "Harp - Putter & Approach",
+    img: "img/Putter5Harp.jpg",
+    price: 12.99,
+    company: "Westside",
+    info:
+      "Designed to withstand any type of condition. It will hold in the wind. For professional players, this could be their only approach disc they will need to carry as it can hold anyone's arm speed and still hyzer. For slower arm speeds, it will be your most overstable approach disc.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 18,
+    title: "Bounty - Putter & Approach",
+    img: "img/Putter6Bounty.jpg",
+    price: 17.99,
+    company: "Dynamic Disc",
+    info:
+      "With a shallow rim and bead, the Bounty is a slightly understable midrange that acts like a slower Maverick. At shorter distances and slower speeds, the Bounty will fly very straight with good glide and minimal fade. When thrown with more speed, the Bounty will slowly move to the right without burning into the ground.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 19,
+    title: "Commander Disc Golf Bag",
+    img: "img/DiscGolfBag.jpg",
+    price: 124.99,
+    company: "Dynamic Disc",
+    info:
+      "With a 20-disc main compartment, you will not sacrifice carrying capacity for a smaller footprint. In addition, the 2 pockets on the vertical wall allow for several more discs to head out to the course. Full length zippered vertical pockets can hold all of your extra gear with room to spare. 2 bottle holders, 2 cell phone sized pockets, chair/umbrella strap, and an incredibly lightweight but durable construction will make this bag a favorite of casual and tournament players alike.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+];
+
+export const detailProduct = {
+  id: 8,
+  title: "Vandal - Fairway Driver",
+  img: "img/Fairway4Vandal.jpg",
+  price: 17.99,
+  company: "Dynamic Disc",
+  info:
+    "The Vandal flies like a longer Maverick, so it’s extremely versatile. Newer players will love the straighter lines they can throw, and players with more arm speed will enjoy it for easy turnovers or rollers. The Vandal is built to carve up any course with dependable turn and sneaky speed.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
